90f9c679bc7200d672930e062fe152ae7fb07ec5
Ras Rossetti <rasrossetti@gmail.com>
2021-12-15 20:44:26 -0600

    End of V.1

--- Overworld.js
+++ Overworld.js
@@ -5,6 +5,47 @@ class Overworld {
         this.ctx = this.canvas.getContext('2d');
     }
     init() {
-        console.log("Hello Overworld", this);
+        const image = new Image();
+        image.onload = () => {
+           this.ctx.drawImage(image, 0, 0);
+        };
+        image.src = "images/maps/DemoLower.png";
+
+        const x = 5;
+        const y = 6;
+
+        const shadow = new Image();
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow,
+                0, //left cut
+                0, //top cut
+                32, //width
+                32, //height
+                x * 16 - 8,
+                y * 16 -18,
+                32,
+                32
+            );
+        };
+        shadow.src = "images/characters/Shadow.png";
+
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0, //left cut
+                0, //top cut
+                32, //width
+                32, //height
+                x * 16 - 8,
+                y * 16 -18,
+                32,
+                32
+            );
+        }
+        hero.src = "images/characters/people/hero.png";
+
+
     }
 }
