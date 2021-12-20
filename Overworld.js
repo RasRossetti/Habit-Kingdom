3f8915ef3de613ef4b892b932eaa77c91906ac54
Ras Rossetti <rasrossetti@gmail.com>
2021-12-19 21:43:30 -0600

    V.2(issues)

--- Overworld.js
+++ Overworld.js
@@ -9,43 +9,21 @@ class Overworld {
         image.onload = () => {
            this.ctx.drawImage(image, 0, 0);
         };
-        image.src = "images/maps/DemoLower.png";
-
-        const x = 5;
-        const y = 6;
-
-        const shadow = new Image();
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow,
-                0, //left cut
-                0, //top cut
-                32, //width
-                32, //height
-                x * 16 - 8,
-                y * 16 -18,
-                32,
-                32
-            );
-        };
-        shadow.src = "images/characters/Shadow.png";
-
-        const hero = new Image();
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero,
-                0, //left cut
-                0, //top cut
-                32, //width
-                32, //height
-                x * 16 - 8,
-                y * 16 -18,
-                32,
-                32
-            );
-        }
-        hero.src = "images/characters/people/hero.png";
-
-
+        image.src = "/images/maps/DemoLower.png";
+        //Pace some Game Objects
+        const hero = new GameObject({
+            x: 5,
+            y: 6,
+        })
+        const npc1 = new GameObject({
+            x: 7,
+            y: 9,
+            src: "/images/characters/people/npc1.png"
+        })
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 200);
+      
     }
 }
